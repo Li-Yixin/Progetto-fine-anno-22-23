01390ed9664c104656e45f95052f0e9fded4b804
Li-Yixin <li.yixin@iisgalvanimi.edu.it>
2023-04-19 10:58:36 +0000

    19/04/2023

--- import_data.py
+++ import_data.py
@@ -15,7 +15,7 @@ mycursor.execute("CREATE DATABASE IF NOT EXISTS SOLAR_SYSTEM")
 
 #Create the table for the csv data (if not exists)
 mycursor.execute("""
-  CREATE TABLE IF NOT EXISTS SOLAR_SYSTEM.Clash_Unit (
+  CREATE TABLE IF NOT EXISTS SOLAR_SYSTEM.System_Planet (
     Planet VARCHAR(30) NOT NULL,    #Color,Mass,Diameter,Density,Surface Gravity
     Color VARCHAR(100),
     Mass FLOAT,
@@ -40,11 +40,11 @@ mycursor.execute("""
   );""")
 
 #Delete data from the table Clsh_Unit
-mycursor.execute("DELETE FROM CLASH_ROYALE.Clash_Unit")
+mycursor.execute("DELETE FROM SOLAR_SYSTEM.System_Planet")
 mydb.commit()
 
 #Read data from a csv file
-clash_data = pd.read_csv('./cr-unit-attributes.csv', index_col=False, delimiter = ',')
+clash_data = pd.read_csv('./planets.csv', index_col=False, delimiter = ',')
 clash_data = clash_data.fillna('Null')
 print(clash_data.head(20))
 
@@ -52,7 +52,7 @@ print(clash_data.head(20))
 for i,row in clash_data.iterrows():
     cursor = mydb.cursor()
     #here %S means string values 
-    sql = "INSERT INTO CLASH_ROYALE.Clash_Unit VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+    sql = "INSERT INTO SOLAR_SYSTEM.System_Planet VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
     cursor.execute(sql, tuple(row))
     print("Record inserted")
     # the connection is not auto committed by default, so we must commit to save our changes
